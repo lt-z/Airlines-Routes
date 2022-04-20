cfe6902e2d712803115c4f0bca7df35e86d28f99
Lawrence Tam <lawrenceatam@gmail.com>
2022-04-19 17:52:58 -0700

    step 5 - added table pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Table = ({ routes, columns, format }) => {
+const Table = ({ routes, columns, format, perPage = 25 }) => {
+  const [page, setPage] = useState(0);
+
   const header = columns.map((col) => {
     return <th key={col.name}>{col.name}</th>;
-  })
+  });
 
-  const body = routes.map((row) => { // for each row... row = obj
+  const previousPage = (event) => {
+    event.preventDefault();
+    setPage(page - 1);
+  };
+
+  const nextPage = (event) => {
+    event.preventDefault();
+    setPage(page + 1);
+  }
+
+  const start = page * perPage;
+  const end = start + perPage;
+
+  const body = routes.slice(start, end).map((row) => { // for each row... row = obj
     const rows = columns.map((col) => { // create the data in 3 cols
       const val = row[col.property];
       return <td key={col.property + val}>{format(col.property, val)}</td>
@@ -22,6 +37,15 @@ const Table = ({ routes, columns, format }) => {
         </thead>
         <tbody>{body}</tbody>
       </table>
+      <div className="pagination">
+        <p>
+          Showing {start + 1}-{end} of {routes.length} routes.
+        </p>
+        <p>
+          <button onClick={previousPage} disabled={page === 0}>Previous Page</button>
+          <button onClick={nextPage} disabled={end >= routes.length}>Next Page</button>
+        </p>
+      </div>
     </div>
   )
 }
